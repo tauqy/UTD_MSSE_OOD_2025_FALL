571840e3e3d8ac30728a005eb880404e40447991
Tauqeer Murtaza <tauqeer.287@hotmail.com>
2025-12-14 23:51:55 -0600

    Fixing Game console purchase strategy

--- movie-rental/Purchase/Strategies/PurchasePrice/Factory/GameConsolePurchaseStrategyFactory.java
+++ movie-rental/Purchase/Strategies/PurchasePrice/Factory/GameConsolePurchaseStrategyFactory.java
@@ -4,13 +4,14 @@ import Item.GamingConsole.GameConsole;
 import Item.Item;
 import Purchase.DiscountPurchase;
 import Purchase.Purchase;
+import Purchase.Strategies.PurchasePrice.GameConsole.GameConsolePurchaseStrategy;
 import Purchase.Strategies.PurchasePrice.VideoGamePurchase.VideoGamePurchaseStrategy;
 
 public class GameConsolePurchaseStrategyFactory {
     public DiscountPurchase getStrategy(Purchase purchase){
         Item item = purchase.getItem();
         if(item instanceof GameConsole){
-            return new VideoGamePurchaseStrategy();
+            return new GameConsolePurchaseStrategy();
         }
         throw new IllegalArgumentException("Unknown Video Game type");
     }
